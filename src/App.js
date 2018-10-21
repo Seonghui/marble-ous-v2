148cadee437333a1e9801ee55fd5f6ed4c8089ea
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-21 22:15:02 +0900

    헤더 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import { MainPage, DesignerPage, ContentPage, AboutPage } from "pages";
-import Menu from "./components/Common/Menu/Menu";
+import Header from "./components/Common/Header/Header";
+import "./styles/reset.css";
+import "./styles/common.scss";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Menu />
+        <Header />
         <Switch>
           <Route exact path="/" component={MainPage} />
           <Route exact path="/designer" component={DesignerPage} />
---
+++ src/components/Common/Header/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Header.scss";
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Header">
+        <a className="logo" href="/">
+          <img src="/images/logo.png" alt="logo" />
+        </a>
+        <ul>
+          <li><Link to="/">WORK</Link></li>
+          <li><Link to="/designer">DESIGNER</Link></li>
+          <li><Link to="/About">ABOUT</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Menu;

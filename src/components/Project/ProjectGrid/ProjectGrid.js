968873d01ec76b0ca9f3aaf8e05ec6a03c600626
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-22 21:11:25 +0900

    배경 그리드 추가

---
+++ src/components/Project/ProjectGrid/ProjectGrid.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./ProjectGrid.scss";
+
+class ProjectGrid extends Component {
+  render() {
+    return (
+      <div className="ProjectGrid">
+      </div>
+    );
+  }
+}
+
+export default ProjectGrid;

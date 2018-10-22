d932352d2034bb5225be6e5cc913080e8e24bc0c
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-22 21:06:55 +0900

    헤더 고정

--- src/components/Common/Header/Header.js
+++ src/components/Common/Header/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./Header.scss";
 
-class Menu extends Component {
+class Header extends Component {
   render() {
     return (
       <div className="Header">
@@ -19,4 +19,4 @@ class Menu extends Component {
   }
 }
 
-export default Menu;
+export default Header;

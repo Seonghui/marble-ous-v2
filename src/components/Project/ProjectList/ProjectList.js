a79847ad5cc690b0ab5f5a3d3cb6eceddf3253db
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-22 21:15:12 +0900

    props 값 추가

--- src/components/Project/ProjectList/ProjectList.js
+++ src/components/Project/ProjectList/ProjectList.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import ProjectItem from '../ProjectItem/ProjectItem';
 
+
 class ProjectList extends Component {
 
   renderProjectItem = () => {
     const projectItem = this.props.data.map((res, index) => {
-      return <ProjectItem key={index} id={index} titleKr={res.titleKr}/>
+      return <ProjectItem key={index} id={index} titleKr={res.titleKr} coX={res.coX} coY={res.coY} />
     })
     return projectItem;
   }

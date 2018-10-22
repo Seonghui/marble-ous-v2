84a20ac4e7e3d5e9eaa0d5eb61b8f62c69671e84
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-22 21:10:54 +0900

    ProjectItem 절대 위치 설정

--- src/components/Project/ProjectItem/ProjectItem.js
+++ src/components/Project/ProjectItem/ProjectItem.js
@@ -1,20 +1,31 @@
 import React, { Component } from "react";
 import "./ProjectItem.scss";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
 
-class Project extends Component {
+class ProjectItem extends Component {
+  
   render() {
-    const { id, titleKr } = this.props;
+    const { id, titleKr, coX, coY } = this.props;
+    const Box = styled.div`
+      position: absolute;
+      top: ${coY * 220}px;
+      left: ${coX * 220}px;
+    `;
     const link = "/content/" + id;
     return (
+      <Box>
       <div className="ProjectItem">
+      
         <Link to={link}>
           <div className="item" />
           <span>{titleKr}</span>
         </Link>
+      
       </div>
+      </Box>
     );
   }
 }
 
-export default Project;
+export default ProjectItem;

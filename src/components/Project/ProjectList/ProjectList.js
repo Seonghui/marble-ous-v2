20109d17af2355ef86522f6f44f6ebc8416a5ae4
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-10-27 20:46:33 +0900

    ProjectItem에 전달하는 props 추가

--- src/components/Project/ProjectList/ProjectList.js
+++ src/components/Project/ProjectList/ProjectList.js
@@ -6,7 +6,15 @@ class ProjectList extends Component {
 
   renderProjectItem = () => {
     const projectItem = this.props.data.map((res, index) => {
-      return <ProjectItem key={index} id={index} titleKr={res.titleKr} coX={res.coX} coY={res.coY} />
+      return <ProjectItem 
+      key={index} 
+      id={index}
+      titleKr={res.titleKr}
+      designer1={res.designer[0].nameKr}
+      designer2={res.designer[1].nameKr}
+      designer3={res.designer[2].nameKr}
+      coX={res.coX}
+      coY={res.coY} />
     })
     return projectItem;
   }

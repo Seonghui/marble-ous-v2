7f36fb3166554ec6db593eddf306b59d34234a74
Seonghui Yugit config --global user.name stellashyou@gmail.com <stellashyou@gmail.com>
2018-11-12 20:36:30 +0900

    메인 캐릭터 구현

--- src/components/Project/ProjectAside/ProjectAside.js
+++ src/components/Project/ProjectAside/ProjectAside.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Panel from '../../Common/Panel/Panel';
+import ProjectMap from '../ProjectMap/ProjectMap';
+import styled from 'styled-components';
 import "./ProjectAside.scss";
 
 class ProjectAside extends Component {
   render() {
-    const { gridX, gridY } = this.props;
+    const { gridX, gridY, data } = this.props;
     return (
       <div className="ProjectAside">
         <Panel title="FILTER">
@@ -14,7 +16,7 @@ class ProjectAside extends Component {
           <Character gridX={gridX} gridY={gridY} />
         </Panel>
         <Panel title="MINI MAP">
-          <h1>미니맵</h1>
+          <ProjectMap data={data} gridX={gridX} gridY={gridY} />
         </Panel>
       </div>
     );
@@ -24,20 +26,30 @@ class ProjectAside extends Component {
 class Character extends Component {
   render() {
     const { gridX, gridY } = this.props;
-    return(
+    const xPos = parseInt(gridX * 188 / 2200, 10);
+    const yPos = parseInt(gridY * 188 / 2200, 10);
+
+    const TopDot = styled.div`
+      left: ${yPos}px;
+    `;
+    const BottomDot = styled.div`
+      left: ${xPos}px;
+    `;
+
+    return (
       <div className="Character">
         <div className="line line-1">
-          <div className="tri"></div>
+          <TopDot className="dot"></TopDot>
           <div className="text">
-            <span>Fun</span>
-            <span>Insightful</span>
+            <span className="fun">Fun</span>
+            <span className="insightful">Insightful</span>
           </div>
         </div>
         <div className="line line-2">
-          <div className="tri"></div>
+          <BottomDot className="dot"></BottomDot>
           <div className="text">
-            <span>Experimental</span>
-            <span>Practical</span>
+            <span className="experimental">Experimental</span>
+            <span className="practical">Practical</span>
           </div>
         </div>
       </div>
